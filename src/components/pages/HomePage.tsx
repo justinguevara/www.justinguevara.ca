3bf01863ea493c1b0b573658f0d9ae3ded2d0eb2
Justin Guevara <k@guevarajust.in>
2025-01-15 23:07:31 -0500

    update about page copy

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,39 +3,37 @@ import MainNavigation from "components/MainNavigation";
 function HomePage (): JSX.Element {
   return (
     <>
-      <MainNavigation/>
+      <MainNavigation />
       <div className="temp"></div>
       <div className="front-page">
 
-    <div className="center-block-element content-width">
-      <div className="feature-container">
-        <div className="home-heading-background">
-        </div>
-      </div>
-      <div className="primary-content-container">
-
-
-                <section className="first-section">
-                    <div className="name">Justin Guevara</div>
-                    <ul className="secondary-font below-name">
-                        <li className="email">j[at]guevarajust[dot]in</li>
-                        <li><a href="https://linkedin.com/in/justinguevara" className="link">linkedin.com/in/justinguevara</a></li>
-                    </ul>
-                    <div className="secondary-font short-description"><span className="bold">Experienced Software Developer </span> based in <span className="bold"> Toronto, Canada</span>.</div>
-                </section>
-
-                <section className="quotes">
-                    <div className="quote">
-                        <div className="content">you were great, and I would highly recommend you to anyone in the industry</div>
-                        <div className="person"><a href="https://torontolife.com/tag/chris-noll/" className="link">Chris Noll</a>, CEO of <a href="https://torontolife.com/food/mill-street-brewery-purchases-torontos-brickworks-ciderhouse">Brickworks Ciderhouse</a></div>
-                    </div>
-                    <div className="quote">
-                        <div className="content">a wonderful developer</div>
-                        <div className="person"><a href="https://www.linkedin.com/in/sunjoyotanto/" className="link">Sunjoyo Tanto</a>, Web Programmer & IT Analyst</div>
-                    </div>
-                </section>
+        <div className="center-block-element content-width">
+          <div className="feature-container">
+            <div className="home-heading-background">
             </div>
           </div>
+          <div className="primary-content-container">
+            <section className="first-section">
+              <div className="name">Justin Guevara</div>
+              <ul className="secondary-font below-name">
+                <li className="email">j[at]guevarajust[dot]in</li>
+                <li><a href="https://linkedin.com/in/justinguevara" className="link">linkedin.com/in/justinguevara</a></li>
+              </ul>
+              <div className="secondary-font short-description"><span className="bold">Experienced Software Developer </span> based in <span className="bold"> Toronto, Canada</span>.</div>
+            </section>
+
+            <section className="quotes">
+              <div className="quote">
+                <div className="content">you were great, and I would highly recommend you to anyone in the industry</div>
+                <div className="person"><a href="https://torontolife.com/tag/chris-noll/" className="link">Chris Noll</a>, CEO of <a href="https://torontolife.com/food/mill-street-brewery-purchases-torontos-brickworks-ciderhouse">Brickworks Ciderhouse</a></div>
+              </div>
+              <div className="quote">
+                <div className="content">a wonderful developer</div>
+                <div className="person"><a href="https://www.linkedin.com/in/sunjoyotanto/" className="link">Sunjoyo Tanto</a>, Web Programmer & IT Analyst</div>
+              </div>
+            </section>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/components/pages/MusicPage.tsx
@@ -0,0 +1,22 @@
+import MainNavigation from "components/MainNavigation";
+
+export default function MusicPage (): JSX.Element {
+  return (
+    <>
+        <MainNavigation/>
+        <div className="front-page">
+            <div className="center-block-element content-width">
+                <div className="primary-content-container">
+                    <section className="first-section">
+                        <h4 style={{fontSize:'1.5em'}}>Hello.</h4>
+
+
+
+
+                    </section>
+                </div>
+            </div>
+        </div>
+    </>
+  );
+};
