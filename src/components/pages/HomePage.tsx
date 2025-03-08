35e9c38ad43744f758beeb5359e179c8b3ee2cb4
Justin Guevara <k@guevarajust.in>
2025-03-08 00:46:22 -0500

    update home page feature

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,11 +1,35 @@
 import MainNavigation from "components/MainNavigation";
+import { useRef, useEffect } from 'react';
+import seedrandom from 'seedrandom';
 
 export default function HomePage (): JSX.Element {
+  const canvas_reference = useRef(null);
+  useEffect(() => {
+    const canvas = canvas_reference.current;
+    const context = canvas.getContext('2d');
+    var random = new seedrandom(seed);
+    for (let y = 0; y < canvas_height; y++) {
+      for (let x = 0; x < canvas_width; x++) {
+        if (normalizeNumber(x, 0, canvas_width)  <  random()) {
+          const random_color = 'rgb(' + color_palette[Math.floor( (random() * color_palette.length) )].join(', ') + ')';
+          context.fillStyle = random_color;
+          context.fillRect(x * pixel_size, y * pixel_size, pixel_size, pixel_size);
+        }
+      }
+    }
+  }, []);
+
   return (
     <>
       <MainNavigation />
       <div className="front-page">
         <div className="center-block-element content-width">
+          <div className="canvas-container">
+            <div className="canvas-inner-container">
+              <canvas className="canvas" ref={canvas_reference} width={canvas_width} />
+            </div>
+          </div>
+
           <div className="primary-content-container">
             <section className="first-section">
               <div className="name">Justin Guevara</div>
@@ -31,3 +55,23 @@ export default function HomePage (): JSX.Element {
     </>
   );
 };
+
+const canvas_height = 198;
+const canvas_width = 1148;
+const pixel_size = 22;
+const seed = '5';
+
+const normalizeNumber = function (value: number, min: number, max: number) {
+  let scaled = (value - min) / (max - min);
+  return 0.95 - scaled * (0.99 - 0.5);
+};
+
+const color_palette = [
+  [34, 54, 98],
+  [38, 120, 113],
+  [231, 118, 47],
+  [245, 192, 67],
+  [221, 210, 190],
+  [108, 81, 135],
+  [51, 51, 51]
+];
