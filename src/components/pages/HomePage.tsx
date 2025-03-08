b5fb53b7fcaab42d1b9448e13001ad7fdeb5911a
Justin Guevara <k@guevarajust.in>
2025-03-08 14:40:37 -0500

    adjust home page canvas draw formula

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -4,13 +4,37 @@ import seedrandom from 'seedrandom';
 
 export default function HomePage (): JSX.Element {
   const canvas_reference = useRef(null);
+
+  const canvas_height = 154;
+  const canvas_width = 1148;
+  const pixel_size = 22;
+  const seed = '5';
+  const random = new seedrandom(seed);
+
   useEffect(() => {
+    // scale number to 0.49 - 0.95 with inversion
+    const normalizeNumber = function (value: number, min: number, max: number): number {
+      let scaled_number = (value - min) / (max - min);
+      return 0.95 - scaled_number * (0.49);
+    };
+
+    const color_palette = [
+      [34, 54, 98],
+      [38, 120, 113],
+      [231, 118, 47],
+      [245, 192, 67],
+      [221, 210, 190],
+      [108, 81, 135],
+      [51, 51, 51]
+    ];
+
     const canvas = canvas_reference.current;
     const context = canvas.getContext('2d');
-    var random = new seedrandom(seed);
+
     for (let y = 0; y < canvas_height; y++) {
       for (let x = 0; x < canvas_width; x++) {
-        if (normalizeNumber(x, 0, canvas_width) < random()) {
+        const probability = normalizeNumber(x, 0, canvas_width);
+        if (probability < random()) {
           const random_color = 'rgb(' + color_palette[Math.floor( (random() * color_palette.length) )].join(', ') + ')';
           context.fillStyle = random_color;
           context.fillRect(x * pixel_size, y * pixel_size, pixel_size, pixel_size);
@@ -55,23 +79,3 @@ export default function HomePage (): JSX.Element {
     </>
   );
 };
-
-const canvas_height = 154;
-const canvas_width = 1148;
-const pixel_size = 22;
-const seed = '5';
-
-const normalizeNumber = function (value: number, min: number, max: number) {
-  let scaled = (value - min) / (max - min);
-  return 0.95 - scaled * (0.99 - 0.5);
-};
-
-const color_palette = [
-  [34, 54, 98],
-  [38, 120, 113],
-  [231, 118, 47],
-  [245, 192, 67],
-  [221, 210, 190],
-  [108, 81, 135],
-  [51, 51, 51]
-];
